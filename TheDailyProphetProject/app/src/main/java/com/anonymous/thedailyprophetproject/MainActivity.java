11f9d15e7d146f3474df1db752dfe10bd8a70ba8
Piyush Pawar <piyushron7@gmail.com>
2019-03-16 22:44:05 +0530

    First Commit

---
+++ TheDailyProphetProject/app/src/main/java/com/anonymous/thedailyprophetproject/MainActivity.java
@@ -0,0 +1,141 @@
+package com.anonymous.thedailyprophetproject;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+    ArFragment arFragment;
+    boolean shouldAddModel = true;
+
+    @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
+        setContentView(R.layout.activity_main);
+
+        arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+        arFragment.getPlaneDiscoveryController().hide();
+
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        Collection<AugmentedImage> augmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : augmentedImages) {
+            if (augmentedImage.getTrackingState() == TrackingState.TRACKING) {
+                if (augmentedImage.getName().equals("car") && shouldAddModel) {
+                    System.out.println("Found image");
+                    placeObject(arFragment, augmentedImage.createAnchor(augmentedImage.getCenterPose()), Uri.parse("car.sfb"));
+                    shouldAddModel = false;
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void placeObject(ArFragment arFragment, Anchor anchor, Uri uri) {
+        System.out.println("Placing Object");
+        ModelRenderable.builder()
+                .setSource(arFragment.getContext(), uri)
+                .build()
+                .thenAccept(modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable))
+                .exceptionally(throwable -> {
+                            Toast.makeText(arFragment.getContext(), "Error:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                            return null;
+                        }
+                );
+    }
+
+    private void addNodeToScene(ArFragment arFragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    public boolean setupAugmentedImagesDb(Config config, Session session) {
+        System.out.println("Setting up Augmented image database");
+        AugmentedImageDatabase augmentedImageDatabase;
+        Bitmap bitmap = loadAugmentedImage();
+        if (bitmap == null) {
+            return false;
+        }
+        augmentedImageDatabase = new AugmentedImageDatabase(session);
+        augmentedImageDatabase.addImage("car", bitmap);
+        config.setAugmentedImageDatabase(augmentedImageDatabase);
+        System.out.println("Augmented image database set up");
+        return true;
+    }
+
+    private Bitmap loadAugmentedImage() {
+        System.out.println("Loading Augmented Images");
+        try (InputStream is = getAssets().open("car_image.png")) {
+            return BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            Log.e("ImageLoad", "IO Exception", e);
+        }
+        return null;
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+}
